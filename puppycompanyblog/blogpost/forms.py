62188f90e68b7e2b7c127b83949e6517648aed3b
betadayz <betadayz888@gmail.com>
2020-09-11 14:26:33 +0100

    setting up blog post form

---
+++ puppycompanyblog/blogpost/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,Submitfield,TextAreaField
+from wtformd.validators import DataRequireed
+
+class BlogPostForm(FlaskForm):
+    title = StringField('Title',validators=[DataRequired()])
+    text = TextAreaField('Text',validators=[DataRequired()])
+    submit = SubmitField('Post')
