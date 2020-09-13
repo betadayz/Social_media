4d2576de7e42971765e98303720ebb43e1a79a9c
betadayz <betadayz888@gmail.com>
2020-09-13 10:29:04 +0100

    views.py

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -23,7 +23,7 @@ login_manager.login_view = 'users.login'
 
 from puppycompanyblog.core.views import core
 from puppycompanyblog.users.views import users
-from  puppycompanyblog.blog_posts.views import blog_posts
+from puppycompanyblog.blog_posts.views import blog_posts
 from puppycompanyblog.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
--- puppycompanyblog/blog_posts/views.py
+++ puppycompanyblog/blog_posts/views.py
@@ -2,7 +2,7 @@ from flask import render_template,url_for,flash,request,redirect,Blueprint
 from flask_login import current_user, login_required
 from puppycompanyblog import db
 from puppycompanyblog.models import BlogPost
-from puppycompanyblog.blog_post.forms import BlogPostForm
+from puppycompanyblog.blog_posts.forms import BlogPostForm
 
 blog_posts = Blueprint('blog_posts',__name__)
 
@@ -26,11 +26,11 @@ def create_post():
 def blog_post(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
     return render_template('blog_post.html',title=blog_post.title,
-                           date=blog_post.date,post=blog_post) 
+                            date=blog_post.date,post=blog_post) 
 
 
 
-@blog_post.route('/<int:blog_post_id/update',methods=['GET','POST'])
+@blog_posts.route('/<int:blog_post_id>/update',methods=['GET','POST'])
 @login_required
 def update(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
@@ -43,13 +43,13 @@ def update(blog_post_id):
         db.session.commit()
         flash('Blog Post Updated')
         return redirect(url_for('blog_posts.blog_post',blog_post_id=blog_post.id))
-    elif request.method = 'GET':
+    elif request.method == 'GET':
         form.title.data = blog_post.title
         form.text.data = blog_post.text
     return render_template('create_post.html',title='Updating',form=form)
 
 
-@blog_post.route('/<int:blog_post_id/delete',methods=['GET','POST'])
+@blog_posts.route('/<int:blog_post_id>/delete',methods=['GET','POST'])
 @login_required
 def delete_post(blog_post_id):
     blog_post = BlogPost.query.get_or_404(blog_post_id)
